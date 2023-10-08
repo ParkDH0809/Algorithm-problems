22374b93e1980326815f0047d798a21b99442850
ParkDH0809 <99964675+ParkDH0809@users.noreply.github.com>
2023-10-08 22:26:14 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.append(br.readLine() + "??!");
+        bw.close();
+    }
+}
