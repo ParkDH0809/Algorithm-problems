9555b9f4736fa4a439676afef2d0365fcbf22315
ParkDH0809 <znxn1237@gmail.com>
2023-12-04 16:04:28 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 55.34 ms, Memory: 185 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,30 +1,29 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
+        Arrays.fill(answer, -1);
+        
         List<Integer> list = new ArrayList<>();
         list.add(numbers[numbers.length - 1]);
-        answer[numbers.length - 1] = -1;
         
-        for(int i = numbers.length - 2; i >= 0; i--) {
-            int size = list.size();
+        for(int i = numbers.length - 2; i >= 0; i--) 
+            getAnswer(answer, numbers, list, i);
         
-            for(int j = size - 1; j >= 0; j--) {
-                int currentNumber = list.get(j);
-                if(numbers[i] < currentNumber) {
-                    answer[i] = currentNumber;
-                    list.add(numbers[i]);
-                    break;
-                } else {
-                    list.remove(j);
-                }
-                
-                if(j == 0)  {
-                    answer[i] = -1;
-                    list.add(numbers[i]);
-                }
+        return answer;
+    }
+    
+    void getAnswer(int[] answer, int[] numbers, List<Integer> list, int i) {
+        for(int j = list.size() - 1; j >= 0; j--) {
+            if(numbers[i] < list.get(j)) {
+                answer[i] = list.get(j);
+                list.add(numbers[i]);
+                return;
+            } else {
+                list.remove(j);
             }
         }
-        return answer;
+        list.add(numbers[i]);
     }
 }
