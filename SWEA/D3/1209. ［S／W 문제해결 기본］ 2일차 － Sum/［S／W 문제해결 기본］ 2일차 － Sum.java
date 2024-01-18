1e4fbad0b5ebe3c143d472eaa595a70f50059502
ParkDH0809 <znxn1237@gmail.com>
2024-01-18 16:55:15 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 152 ms, Memory: 31,900 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,50 +1,61 @@
 import java.util.*;
 import java.io.*;
 
-class Solution {
-	public static void main(String args[]) throws Exception {
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
 		int T = 10;
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-            br.readLine();
+			br.readLine();
             
-			int[][] arr = new int[100][100];
-			for(int i = 0; i < 100; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				for(int j = 0; j < 100; j++) {
-					arr[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			//가로, 세로
-			int answer = Integer.MIN_VALUE;
-			int sum = 0;
-			int sum2 = 0;
-			for(int i = 0; i < 100; i++) {
-                sum = sum2 = 0;
-				for(int j = 0; j < 100; j++) {
-					sum += arr[i][j];
-					sum2 += arr[j][i];
-				}
-				answer = Math.max(sum,  answer);
-				answer = Math.max(sum2,  answer);
-			}
-			
-			//대각선
-			sum = 0;
-			sum2 = 0;
-			for(int i = 0; i < 100; i++) {
-				sum += arr[i][i];
-				sum2 += arr[99 - i][i];
-			}
-			answer = Math.max(sum,  answer);
-			answer = Math.max(sum2,  answer);
-			
-			bw.append("#" + test_case + " "+ answer + "\n");
+            int[][] board = new int[100][100];
+            for(int i = 0; i < 100; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                for(int j = 0; j < 100; j++) {
+                    board[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            
+            bw.append("#" + test_case + " " +getMax(board));
+            bw.newLine();
 		}
-        bw.close();
+        bw.flush();
 	}
+    
+    static int getMax(int[][] board) {
+        int max = 0;
+        
+        // 가로
+        for(int i = 0; i < 100; i++) {
+            int sum = 0;
+            for(int j = 0; j < 100; j++) {
+                sum += board[i][j];
+            }
+            max = Math.max(sum, max);
+        }
+        
+        // 세로
+        for(int i = 0; i < 100; i++) {
+            int sum = 0;
+            for(int j = 0; j < 100; j++) {
+                sum += board[j][i];
+            }
+            max = Math.max(sum, max);
+        }
+        
+        // 대각선
+		int sum = 0;
+        int sum2 = 0;
+        for(int i = 0; i < 100; i++) {
+            sum += board[i][i];
+            sum2 += board[i][99-i];
+        }
+        
+        return Math.max(max, Math.max(sum, sum2));
+    }
+
 }
