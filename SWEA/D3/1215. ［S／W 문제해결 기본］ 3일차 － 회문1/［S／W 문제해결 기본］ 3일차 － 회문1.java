785e3416c9f7f667ed86dff7c8b86489508740ee
ParkDH0809 <znxn1237@gmail.com>
2024-01-19 09:39:53 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 101 ms, Memory: 18,372 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
@@ -1,54 +1,61 @@
 import java.util.*;
 import java.io.*;
 
-class Solution {
-	public static void main(String args[]) throws Exception {
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
 		int T = 10;
-
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
 			int n = Integer.parseInt(br.readLine());
-			char[][] arr = new char[8][8]; 
-			for(int i = 0; i < 8; i++) {
-				arr[i] = br.readLine().toCharArray();
-			}
-			
-			int answer = 0;
-			answer += method(arr, n);
-			
-			char[][] tmp = new char[8][8];
-			for(int i = 0; i < 8; i++) {
-				for(int j = 0; j < 8; j++) {
-					tmp[i][j] = arr[j][i];
-				}
-			}
-			answer += method(tmp, n);
-			bw.append("#" + test_case + " "+ answer + "\n");
+            char[][] board = new char[8][8];
+            for(int i = 0; i < 8; i++) {
+                board[i] = br.readLine().toCharArray();
+            }
+            
+            int answer = 0;
+            answer += check(board, n);
+            board = turnBoard(board);
+            answer += check(board, n);
+            
+            bw.append("#" + test_case + " " + answer);
+            bw.newLine();
 		}
-        bw.close();
+        bw.flush();
 	}
-	
-	
-	static int method(char[][] arr, int n) {
-		int answer = 0;
-		for(int i = 0; i < 8; i++) {
-			for(int j = 0; j < 8 - n + 1; j++) {
-				if(palindrome(Arrays.copyOfRange(arr[i], j, j + n))) {
-					answer++;
-				}
-			}
-		}
-		return answer;
-	}
-	
-	static boolean palindrome(char[] arr) {
-		for(int i = 0; i < arr.length / 2; i++) {
-			if(arr[i] != arr[arr.length - i - 1]) {
-				return false;				
-			}
-		}
-		return true;
-	}
+    
+    static int check(char[][] board, int n) {
+        int count =0;
+        for(int i=0; i < 8; i++) {
+            for(int j=0; j <= 8-n; j++) {
+                if(isPalindrome(Arrays.copyOfRange(board[i], j, j+n))) {
+                    count++;
+                }
+            }
+        }
+        return count;       
+    }
+    
+    static boolean isPalindrome(char[] arr) {
+        for(int i =0 ; i < arr.length / 2; i++) {
+            if(arr[i] != arr[arr.length - (i+1)]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    static char[][] turnBoard(char[][] board) {
+        char[][] changed = new char[8][8];
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                changed[i][j] = board[j][8-(i+1)];
+            }
+        }
+        return changed;
+    }
 }
