0f740ebac2407e0d844b24ed20cf92c6718a7ed6
ParkDH0809 <znxn1237@gmail.com>
2024-02-06 11:26:55 +0900

    [Silver IV] Title: 큐, Time: 140 ms, Memory: 17068 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,62 +1,139 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException  {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        Queue<String> queue = new LinkedList<>();
-       
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        String back = "";
-        for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
 
-            switch(st.nextToken()) {
-                case "push":
-                    back = st.nextToken();
-                    queue.add(back);
-                    break;
-                    
-                case "pop":
-                    if(queue.isEmpty()) {
-                        bw.append("-1\n");
-                    } else {
-                        bw.append(queue.poll() + "\n");
-                    }
-                    break;
-                    
-                case "size":
-                    bw.append(queue.size() + "\n");
-                    break;
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		Queue queue = new Queue();
+		int T = Integer.parseInt(br.readLine());
+		while(T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			switch(st.nextToken()) {
+			case "push": 
+				queue.enque(Integer.parseInt(st.nextToken()));
+				break;
+				
+			case "pop":
+				sb.append(queue.deque() + "\n");
+				break;
+				
+			case "front":
+				sb.append(queue.getFirst() + "\n");
+				break;
+				
+			case "back":
+				sb.append(queue.getLast() + "\n");
+				break;
+				
+			case "empty":
+				if(queue.isEmpty()) {
+					sb.append("1\n");
+				} else {
+					sb.append("0\n");
+				}
+				break;
+				
+			case "size":
+				sb.append(queue.size() + "\n");
+				break;
+			}
+		}
+		System.out.println(sb);
+	}
+	
+}
 
-                case "empty":
-                    if(queue.isEmpty()) {
-                        bw.append("1\n");
-                    } else {
-                        bw.append("0\n");
-                    }
-                    break;
-                
-                case "front":
-                    if(queue.isEmpty()) {
-                        bw.append("-1\n");
-                    } else {
-                        bw.append(queue.peek() + "\n");
-                    }
-                    break;
-                
-                case "back": 
-                    if(queue.isEmpty()) {
-                        bw.append("-1\n");
-                    } else {
-                        bw.append(back + "\n");
-                    }
-                    break;
-            }
-        }
-        bw.close();
-    }
+
+class Node {
+	
+	Node beforeNode;
+	Node afterNode;
+	int value;
+	
+	Node(int value) {
+		this.value = value;
+		this.beforeNode = null;
+		this.afterNode = null;
+	}
+}
+
+
+class Queue {
+	
+	private Node firstNode;
+	private Node lastNode;
+	private int size;
+	
+	public Queue() {
+		firstNode = null;
+		lastNode = null;
+		size = 0;
+	}
+	
+	
+	public boolean isEmpty() {
+		return size == 0 ? true : false;
+	}
+	
+	
+	public int size() {
+		return size;
+	}
+	
+	
+	public int getFirst() {
+		if(isEmpty()) {
+			return -1;
+		}
+		return firstNode.value;
+	}
+	
+
+	public int getLast() {
+		if(isEmpty()) {
+			return -1;
+		}
+		
+		return lastNode.value;
+	}
+	
+	
+	public void enque(int input) {
+		if(isEmpty()) {
+			firstNode = lastNode = new Node(input);
+			size++;
+			return;
+		}
+		
+		lastNode.afterNode = new Node(input);
+		lastNode.afterNode.beforeNode = lastNode;
+		lastNode = lastNode.afterNode;
+		size++;
+	}
+	
+	
+	public int deque() {
+		if(isEmpty()) {
+			return -1;
+		}
+		
+		Node tmp = firstNode;
+		if(size == 1) {
+			firstNode = lastNode = null;
+		} else {
+			firstNode.afterNode.beforeNode = null;
+			firstNode = firstNode.afterNode;
+			tmp.afterNode = null;
+		}
+		size--;
+		return tmp.value;
+	}
+	
 }
