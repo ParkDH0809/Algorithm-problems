47732de63a20e3c28bb2fc91c0e154f112a753b2
ParkDH0809 <znxn1237@gmail.com>
2024-02-08 09:57:35 +0900

    [Silver I] Title: IOIOI, Time: 188 ms, Memory: 20692 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,23 +1,54 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        String S = br.readLine();
-        String string = "IO".repeat(N) + "I";
-        
-        int answer = 0;
 
-        for(int i = 0; i <= M - string.length(); i++) {
-            if(S.substring(i).startsWith(string))
-                answer++;
-        }
+	public static void main(String[] args) throws IOException {
 
-        bw.append(answer + "");
-        bw.flush();
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		String S = br.readLine();
+
+		int answer = 0;
+		int current = 0;
+		boolean iFlag = false;
+		boolean oFlag = false;
+		for (int i = 0; i < M; i++) {
+			if (S.charAt(i) == 'I') {
+
+				if (iFlag) {
+					current = 0;
+				}
+
+				if (oFlag) {
+					current++;
+					oFlag = false;
+				}
+
+				iFlag = true;
+
+			} else {
+				if (oFlag) {
+					current = 0;
+					iFlag = false;
+					oFlag = false;
+				}
+
+				if (iFlag) {
+					iFlag = false;
+					oFlag = true;
+				}
+			}
+
+			if (current == N) {
+				answer++;
+				current--;
+			}
+		}
+
+		System.out.println(answer);
+	}
 }
