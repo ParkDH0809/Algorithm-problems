d00c3d0c4153a4b96670ec32ee3cc194604f2417
ParkDH0809 <znxn1237@gmail.com>
2024-02-15 09:16:10 +0900

    [Gold IV] Title: 알파벳, Time: 1608 ms, Memory: 293196 KB -BaekjoonHub

---
+++ 백준/Gold IV/1987. 알파벳/알파벳.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int R;
+	static int C;
+	static int answer;
+	static char[][] board;
+	static boolean[][] visited;
+	static boolean[] selected;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		board = new char[R][C];
+		for(int i = 0; i < R; i++) {
+			board[i] = br.readLine().toCharArray(); 
+		}
+		visited = new boolean[R][C];
+		selected = new boolean[26];
+		
+		selected[board[0][0] - 'A'] = true;
+		visited[0][0] = true;
+		getAnswer(0, 0, 1);
+		
+		System.out.println(answer);
+	}
+	
+	static void getAnswer(int r, int c, int count) {
+		answer = Math.max(answer, count);
+		
+		int[] dr = {0, 1, 0, -1};
+		int[] dc = {1, 0, -1, 0};
+		
+		for(int dir = 0; dir < 4; dir++) {
+			int nr = r + dr[dir];
+			int nc = c + dc[dir];
+			
+			if(nr < 0 || nr == R || nc < 0 || nc == C || visited[nr][nc] || selected[board[nr][nc] - 'A']) {
+				continue;
+			}
+			
+			visited[nr][nc] = true;
+			selected[board[nr][nc] - 'A'] = true;
+			getAnswer(nr, nc, count+1);
+			visited[nr][nc] = false;
+			selected[board[nr][nc] - 'A'] = false;
+			
+		}
+	}
+}
