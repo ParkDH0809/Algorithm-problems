aed3489f6b5108a77aebec19405bc466200caa4a
ParkDH0809 <znxn1237@gmail.com>
2024-02-19 23:54:08 +0900

    [Silver V] Title: 3의 배수, Time: 212 ms, Memory: 48232 KB -BaekjoonHub

---
+++ 백준/Silver/1769. 3의 배수/3의 배수.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        getAnswer(input);
+    }
+
+    public static void getAnswer(String input){
+        int count = 0;
+        while(true){
+            if(input.length()==1) break;
+
+            long sum=0;
+            for(int i=0;i< input.length();i++){
+                sum+=Long.parseLong(String.valueOf(input.charAt(i)));
+            }
+            count++;
+            input=String.valueOf(sum);
+        }
+
+        System.out.println(count);
+        if(Long.parseLong((input))%3==0){
+            System.out.println("YES");
+        }else{
+            System.out.print("NO");
+        }
+    }
+
+}
