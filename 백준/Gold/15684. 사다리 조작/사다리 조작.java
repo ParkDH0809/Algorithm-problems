ebb2f3fea5c329ff1256b3cca3413c0e9207e452
ParkDH0809 <znxn1237@gmail.com>
2024-02-25 22:25:43 +0900

    [Gold III] Title: 사다리 조작, Time: 984 ms, Memory: 13724 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,19 +15,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());   // 세로선
+        M = Integer.parseInt(st.nextToken());   // 가로선
+        H = Integer.parseInt(st.nextToken());   // 각각의 세로선마다 가로선을 놓을 수 있는 위치의 개수
 
-        ladder = new int[N+1][H+1];
+        ladder = new int[H+1][N+1];
         for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            ladder[b][a] = b+1;
-            ladder[b+1][a] = b;
+            ladder[a][b] = b+1;
+            ladder[a][b+1] = b;
         }
-
+        
         for(int i = 0; i <= 3; i++) {
             getAnswer(0, i);
             if(flag) {
@@ -52,11 +53,11 @@ public class Main {
 
         for(int i = 1; i <= H; i++) {
             for(int j = 1; j < N; j++) {
-                if(ladder[j][i] == 0 && ladder[j+1][i] == 0) {
-                    ladder[j][i] = j+1;
-                    ladder[j+1][i] = j;
+                if(ladder[i][j] == 0 && ladder[i][j+1] == 0) {
+                    ladder[i][j] = j+1;
+                    ladder[i][j+1] = j;
                     getAnswer(count+1, max);
-                    ladder[j][i] = ladder[j+1][i] = 0;
+                    ladder[i][j] = ladder[i][j+1] = 0;
                 }
             }
             
@@ -67,8 +68,8 @@ public class Main {
         for(int i = 1; i <= N; i++) {
             int current = i;
             for(int h = 1; h <= H; h++) {
-                if(ladder[current][h] != 0) {
-                    current = ladder[current][h];
+                if(ladder[h][current] != 0) {
+                    current = ladder[h][current];
                 }
             }
             if(i != current) {
