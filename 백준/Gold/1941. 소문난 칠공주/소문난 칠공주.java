e6d2a293d41a2fa559a10bb1b10e985eb71bfa98
ParkDH0809 <znxn1237@gmail.com>
2024-03-31 13:38:11 +0900

    [Gold III] Title: 소문난 칠공주, Time: 564 ms, Memory: 243352 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -2,92 +2,74 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 
 public class Main {
 
-    static List<Point> sPoints;
-    static List<Point> yPoints;
-    static Point[] sResult;
-    static Point[] yResult;
+    static char[][] map;
     static int answer;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        sPoints = new ArrayList<>();
-        yPoints = new ArrayList<>();
-
+        map = new char[5][5];
         for(int i = 0; i < 5; i++) {
             String input = br.readLine();
             for(int j = 0; j < 5; j++) {
-                if(input.charAt(j) == 'Y') {
-                    yPoints.add(new Point(i, j));
-                } else {
-                    sPoints.add(new Point(i, j));
-                }
+                map[i][j] = input.charAt(j);
             }
         }
 
-        for(int i = 4; i <= 7; i++) {
-            if(sPoints.size() < i) {
-                break;
-            }
-            sResult = new Point[i];
-            yResult = new Point[7-i];
-            combS(0, 0);
-        }
-        
+        comb(new int[7], 0, 0, 0);
         System.out.print(answer);
     }
 
-    static void combS(int start, int count) {
-        if(count == sResult.length) {
-            combY(0, 0);
-            return;
-        }
+    static void comb(int[] selected, int start, int count, int sCount) {
+        if(count == 7) {
+            if(sCount < 4) {
+                return;
+            }
 
-        for(int i = start; i < sPoints.size(); i++) {
-            sResult[count] = sPoints.get(i);
-            combS(i+1, count+1);
-        }
-    }
-
-    static void combY(int start, int count) {
-        if(count == yResult.length) {
-            if(isAnswer()) {
+            if(isAnswer(selected)) {
                 answer++;
             }
             return;
         }
 
-        for(int i = start; i < yPoints.size(); i++) {
-            yResult[count] = yPoints.get(i);
-            combY(i+1, count+1);
+        for(int i = start; i < 25; i++) {
+            int r = i / 5;
+            int c = i % 5;
+            selected[count] = i;
+            if(map[r][c] == 'S') {
+                comb(selected, i+1, count+1, sCount+1);
+            } else {
+                comb(selected, i+1, count+1, sCount);
+            }
         }
     }
 
-    static boolean isAnswer() {
+    static boolean isAnswer(int[] selected) {
         boolean[][] visited = new boolean[5][5];
-        boolean[][] selected = new boolean[5][5];
-        initVisited(selected, sResult);        
-        initVisited(selected, yResult);        
+        boolean[][] result = new boolean[5][5];
+        for(int i = 0; i < selected.length; i++) {
+            result[selected[i] / 5][selected[i] % 5] = true;
+        }
 
         int[] dr = {0, 1, 0, -1};
         int[] dc = {1, 0, -1, 0};
-
-        Queue<Point> queue = new ArrayDeque<>();
-        queue.add(sResult[0]);
-        visited[sResult[0].r][sResult[0].c] = true;
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(selected[0]);
+        visited[selected[0] / 5][selected[0] % 5] = true;
         int count = 0;
         while(!queue.isEmpty()) {
-            Point point = queue.poll();
+            int current = queue.poll();
+            int r = current / 5;
+            int c = current % 5;
 
             for(int dir = 0; dir < 4; dir++) {
-                int nr = point.r + dr[dir];
-                int nc = point.c + dc[dir];
+                
+                int nr = r + dr[dir];
+                int nc = c + dc[dir];
 
                 if(!isRange(nr, nc)) {
                     continue;
@@ -97,9 +79,9 @@ public class Main {
                     continue;
                 }
 
-                if(selected[nr][nc]) {
+                if(result[nr][nc]) {
                     visited[nr][nc] = true;
-                    queue.add(new Point(nr, nc));
+                    queue.add(nr*5 + nc);
                 }
             }
 
@@ -112,29 +94,7 @@ public class Main {
         return false;
     }
 
-    static void initVisited(boolean[][] visited, Point[] points) {
-        for(Point point : points) {
-            visited[point.r][point.c] = true;
-        }
-    }
-
     static boolean isRange(int r, int c) {
         return r >= 0 && r < 5 && c >= 0 && c < 5;
-    }
-}
-
-class Point {
-
-    int r;
-    int c;
-
-    Point(int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
-
-    @Override
-    public String toString() {
-        return r + " " + c;
     }
 }
