aa79f6b5d496b696f588f2831d49458fdbc75cfe
ParkDH0809 <znxn1237@gmail.com>
2024-06-14 15:55:47 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 588 ms, Memory: 352956 KB -BaekjoonHub

---
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -0,0 +1,134 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int M;
+	static char[][] board;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		board = new char[N][M];
+
+		Ball[] ballPoints = new Ball[2];
+		for (int r = 0; r < N; r++) {
+			String input = br.readLine();
+			for (int c = 0; c < M; c++) {
+				if (input.charAt(c) == 'R') {
+					ballPoints[0] = new Ball(r, c, true);
+					board[r][c] = '.';
+					continue;
+				}
+
+				if (input.charAt(c) == 'B') {
+					ballPoints[1] = new Ball(r, c, false);
+					board[r][c] = '.';
+					continue;
+				}
+
+				board[r][c] = input.charAt(c);
+			}
+		}
+
+		System.out.print(bfs(ballPoints));
+	}
+
+	static int bfs(Ball[] ballPoints) {
+		Queue<Ball[]> queue = new ArrayDeque<>();
+		queue.add(ballPoints);
+
+		int count = 1;
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			while (size-- > 0) {
+				Ball[] current = queue.poll();
+				for (int dir = 0; dir < 4; dir++) {
+					Ball[] movePoints = new Ball[2];
+					movePoints[0] = new Ball(current[0].r, current[0].c, true);
+					movePoints[1] = new Ball(current[1].r, current[1].c, false);
+
+					move(movePoints[0], movePoints[1], dir);
+					move(movePoints[1], movePoints[0], dir);
+					move(movePoints[0], movePoints[1], dir);
+
+					if (movePoints[1].isComplete) {
+						continue;
+					}
+
+					if (movePoints[0].isComplete) {
+						return count;
+					}
+
+					queue.add(movePoints);
+				}
+			}
+
+			count++;
+			if (count == 11) {
+				break;
+			}
+		}
+
+		return -1;
+	}
+
+	static void move(Ball curBall, Ball difBall, int dir) {
+		int[] dr = { 0, 1, 0, -1 };
+		int[] dc = { 1, 0, -1, 0 };
+
+		if (curBall.isComplete) {
+			return;
+		}
+
+		while (true) {
+			int nr = curBall.r + dr[dir];
+			int nc = curBall.c + dc[dir];
+
+			if (board[nr][nc] == '#') {
+				break;
+			}
+
+			if (nr == difBall.r && nc == difBall.c) {
+				break;
+			}
+
+			curBall.r = nr;
+			curBall.c = nc;
+
+			if (board[curBall.r][curBall.c] == 'O') {
+				curBall.isComplete = true;
+				curBall.r = 0;
+				curBall.c = 0;
+				break;
+			}
+		}
+	}
+
+}
+
+class Ball {
+
+	int r;
+	int c;
+	boolean isRed;
+	boolean isComplete;
+
+	Ball(int r, int c, boolean isRed) {
+		this.r = r;
+		this.c = c;
+		this.isRed = isRed;
+	}
+
+	public String toString() {
+		return r + " " + c;
+	}
+
+}
