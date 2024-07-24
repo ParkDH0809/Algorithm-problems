66da294ae31543b6d89b5a1b328ba3b63c597a7f
ParkDH0809 <znxn1237@gmail.com>
2024-07-24 23:58:17 +0900

    [Bronze V] Title: 자동완성, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+		if (input.equals("N") || input.equals("n")) {
+			System.out.print("Naver D2");
+		} else {
+			System.out.print("Naver Whale");
+		}
+	}
+
+}
